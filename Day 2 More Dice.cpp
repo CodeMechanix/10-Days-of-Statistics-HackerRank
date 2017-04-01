e3d28e89331076a9ee92190fe15621397a5eeaba
Hasan Mahmud <hasan.357cse@gmail.com>
2017-04-01 10:27:38 +0600

    Add files via upload

---
+++ Day 2 More Dice.cpp
@@ -0,0 +1,14 @@
+/*
+Day 2: More Dice
+
+Objective
+In this challenge, we practice calculating probability. We recommend you review the previous challenge's Tutorial before attempting this problem.
+
+Task
+In a single toss of  fair (evenly-weighted) six-sided dice, find the probability that the values rolled by each die will be different and the two dice have a sum of .
+1 / 9  [ Right ]
+1 / 6
+2 / 3
+ 5 / 6
+
+*/
