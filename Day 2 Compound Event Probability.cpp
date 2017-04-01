9b1ca7aff2963a125bada85879771b243288475d
Hasan Mahmud <hasan.357cse@gmail.com>
2017-04-01 10:29:45 +0600

    Add files via upload

---
+++ Day 2 Compound Event Probability.cpp
@@ -0,0 +1,21 @@
+/*
+Day 2: Compound Event Probability
+
+Objective
+In this challenge, we practice calculating the probability of a compound event. We recommend you review today's Probability Tutorial before attempting this challenge.
+
+Task
+There are  urns labeled , , and .
+
+Urn  contains  red balls and  black balls.
+Urn  contains  red balls and  black balls.
+Urn  contains  red balls and  black balls.
+
+One ball is drawn from each of the  urns. What is the probability that, of the  balls drawn,  are red and  is black?
+
+i. 10 / 63
+ii. 2 / 7
+iii. 17 / 42 [Right]
+iv. 31 / 126
+
+*/
