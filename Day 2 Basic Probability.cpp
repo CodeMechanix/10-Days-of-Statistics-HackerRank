5fc2c4c7b282e70ae8c197de57f25ea78a20807d
Hasan Mahmud <hasan.357cse@gmail.com>
2017-04-01 10:26:00 +0600

    Add files via upload

---
+++ Day 2 Basic Probability.cpp
@@ -0,0 +1,14 @@
+/*
+Day 2: Basic Probability
+
+Objective:
+In this challenge, we practice calculating probability. Check out the Tutorial tab for a breakdown of probability fundamentals!
+
+Task :
+In a single toss of  fair (evenly-weighted) six-sided dice, find the probability that their sum will be at most .
+i. 2 / 3
+ii. 5 / 6 [ Right ]
+iii. 1 / 4
+iv. 1 / 6
+
+*/
