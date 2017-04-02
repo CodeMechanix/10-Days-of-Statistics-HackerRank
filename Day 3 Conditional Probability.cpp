aa2d13e0aa2f25d0b6743fa90e0afbf523872e28
Hasan Mahmud <hasan.357cse@gmail.com>
2017-04-02 22:40:04 +0600

    Add files via upload

---
+++ Day 3 Conditional Probability.cpp
@@ -0,0 +1,12 @@
+/*
+Objective:
+In this challenge, we get started with conditional probability. Check out the Tutorial tab for learning materials!
+
+Task :
+Suppose a family has  children, one of which is a boy. What is the probability that both children are boys?
+
+i. 1 / 3 [Right]
+ii. 1 / 2
+iii. 2 / 3
+iv. 1 / 9
+*/
